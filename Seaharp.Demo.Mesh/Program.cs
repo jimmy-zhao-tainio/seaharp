c5cc39cd534b6e0b9b8f2c1476848c3dfa7883ec
Jimmy Zhao <jimmy@example.com>
2025-11-15 08:49:35 +0100

    Add .editorconfig and refactor mesh demo to use World

--- Seaharp.Demo.Mesh/Program.cs
+++ Seaharp.Demo.Mesh/Program.cs
@@ -1,5 +1,5 @@
 using Seaharp.Geometry;
-using Seaharp.IO;
+using Seaharp.World;
 
 internal static class Program
 {
@@ -9,17 +9,22 @@ internal static class Program
         var aCenter = new Point(0, 0, 0);
         var bCenter = new Point(150, 50, -30);
 
-        var a = new Seaharp.World.Sphere(r, subdivisions: 3, center: aCenter);
-        var b = new Seaharp.World.Sphere(r, subdivisions: 3, center: bCenter);
+        var a = new Sphere(r, subdivisions: 3, center: aCenter);
+        var b = new Sphere(r, subdivisions: 3, center: bCenter);
 
-        var surfaceA = a.Mesh;
-        var surfaceB = b.Mesh;        
+        var world = new World();
+        world.Add(a);
+        world.Add(b);
+
         // Intersection demo temporarily disabled; writing both meshes as-is.
-        var combined = new List<Triangle>(surfaceA.Triangles.Count + surfaceB.Triangles.Count);
-        combined.AddRange(surfaceA.Triangles);
-        combined.AddRange(surfaceB.Triangles);
+        var triangleCount = 0;
+        foreach (var shape in world.Shapes)
+        {
+            triangleCount += shape.Mesh.Triangles.Count;
+        }
+
         var outPath = "spheres_with_disc.stl";
-        StlWriter.Write(combined, outPath);
-        Console.WriteLine($"Wrote placeholder (no intersection): {System.IO.Path.GetFullPath(outPath)} with {combined.Count} triangles");
+        world.Save(outPath);
+        Console.WriteLine($"Wrote placeholder (no intersection): {System.IO.Path.GetFullPath(outPath)} with {triangleCount} triangles");
     }
 }
